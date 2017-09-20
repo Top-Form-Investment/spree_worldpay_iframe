7844bbbb3ff7904d8a0188c42193a8ffd7f21f72
Kaushal <Kaushal>
2017-09-20 11:23:51 +0530

    Updated payment method listing to add merchant code info

---
+++ app/overrides/spree/admin/payment_methods/index.rb
@@ -0,0 +1,12 @@
+Deface::Override.new(:virtual_path => "spree/admin/payment_methods/index",
+                     :name => "add_merchant_code_header",
+                     :insert_after => "[data-hook='admin_payment_methods_index_headers'] th[3]",
+                     :text => %Q{
+                      <th> Merchant Code </th>"
+                     })
+Deface::Override.new(:virtual_path => "spree/admin/payment_methods/index",
+                     :name => "add_merchant_code_row",
+                     :insert_after => "[data-hook='admin_payment_methods_index_rows'] td[3]",
+                     :text => %Q{
+                        <td class="align-center"><%= method.preferences[:merchant_code] %></td>
+                     })
