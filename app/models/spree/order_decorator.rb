34cc58087794cabc8fc44ff55ec05507c4e37286
Kaushal <Kaushal>
2017-09-22 16:25:12 +0530

    Updated order decortor to add callback method

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,10 +1,20 @@
 Spree::Order.class_eval do
 
+  after_save :create_payment_information
+
   # Return available payment methods
   def available_payment_methods
     @available_payment_methods ||= Spree::PaymentMethod.available_on_front_end
     country_iso = self.billing_address.country.iso3
     @available_payment_methods.select{|s| s.type != 'Spree::Gateway::WorldpayIframe' || s.eligible?(self.currency, country_iso)}
+  end
+
+  # Update payment information on order complete
+  def create_payment_information
+    if self.state == 'complete' && self.state_was != 'complete'
+      notifiy = Spree::WorldpayNotification.where(order_id: self.id).first
+      notifiy.handle! if notifiy.present?
+    end
   end
 
 end
