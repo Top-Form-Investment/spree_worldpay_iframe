78fe10d892f9aeca0067e38000c8f92b41a2d797
Kaushal <Kaushal>
2017-09-25 13:04:54 +0530

    Updated config initializer file to remove unuse code

--- config/initializers/spree.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.spree.payment_methods << Spree::Gateway::WorldpayIframe
-WORLDPAY_MERCHANT_CODE = YAML.load_file("#{::Rails.root}/config/worldpay_merchant_code.yml")[::Rails.env]
-WORLDPAY_MERCHANT_COUNTRY = YAML.load_file("#{::Rails.root}/config/worldpay_country_code.yml")
-WORLDPAY_MERCHANT_ENTITY = YAML.load_file("#{::Rails.root}/config/worldpay_merchant_entity.yml")
-
-
