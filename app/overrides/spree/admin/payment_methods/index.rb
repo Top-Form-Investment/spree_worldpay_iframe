bdc2e59643fbdc9d747d9f348633ebc04000e8f8
Kaushal <Kaushal>
2017-10-11 17:45:55 +0530

    Updated payment page to add loader

--- app/overrides/spree/admin/payment_methods/index.rb
+++ app/overrides/spree/admin/payment_methods/index.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => "spree/admin/payment_methods/index",
                      :name => "add_merchant_code_header",
                      :insert_after => "[data-hook='admin_payment_methods_index_headers'] th[3]",
                      :text => %Q{
-                      <th> Merchant Code </th>"
+                      <th> Merchant Code </th>
                      })
 Deface::Override.new(:virtual_path => "spree/admin/payment_methods/index",
                      :name => "add_merchant_code_row",
--- app/overrides/spree/checkout/_payment.rb
+++ app/overrides/spree/checkout/_payment.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/checkout/_payment',
   name: 'Add worldpay script on payment page',
-  insert_after: '[data-hook="payment_fieldset_wrapper"]',
+  insert_before: '[data-hook="payment_fieldset_wrapper"]',
   text: '<%= render "spree/checkout/payment/worldpay_checkout"%>'
 )
