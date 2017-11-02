d5d002a512eadb665c54118b846ea5bc2b2b2870
kaushal <kaushal@supernova.xyz>
2017-11-02 12:17:23 +0530

    Updated payment page to show merchant address view

--- app/assets/javascripts/spree/frontend/worldpay_iframe.js
+++ app/assets/javascripts/spree/frontend/worldpay_iframe.js
@@ -2,8 +2,10 @@ function hide_order_now(obj){
     if($(obj).parent().attr('data-payment-type') == 'Spree::Gateway::WorldpayIframe'){
         $("form#checkout_form_payment").find("input[type='submit']").addClass('hide');
         open_payment_page($(obj).val());
+        $('#worldpay-entity-address').show();
     }else{
         $("form#checkout_form_payment").find("input[type='submit']").removeClass('hide');
+        $('#worldpay-entity-address').hide();
     }
 }
 
@@ -13,5 +15,7 @@ function open_payment_page(method_id){
         //initialise the library and pass options
         libraryObject = new WPCL.Library();
         libraryObject.setup(customOptions[method_id]);
+        console.log(customOptions);
+        $('#worldpay-entity-address p').html(customOptions[method_id]['entityaddress']);
     }
 }
