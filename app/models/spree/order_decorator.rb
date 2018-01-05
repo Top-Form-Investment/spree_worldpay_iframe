a2c147722788b38411a75da3ad219e780289ed01
kaushal <kaushal@supernova.xyz>
2018-01-05 16:27:11 +0530

    Updated available_payment_methods method to resolve issue spree version

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,7 +4,7 @@ Spree::Order.class_eval do
 
   # Return available payment methods
   def available_payment_methods
-    @available_payment_methods ||= PaymentMethod.available(:front_end).order(:updated_at)
+    @available_payment_methods ||= (Spree::PaymentMethod.available(:front_end) + Spree::PaymentMethod.available(:both)).uniq
     country_iso = self.billing_address.country.iso3
     @available_payment_methods.select{|s| s.type != 'Spree::Gateway::WorldpayIframe' || s.eligible?(self.currency, country_iso)}
   end
