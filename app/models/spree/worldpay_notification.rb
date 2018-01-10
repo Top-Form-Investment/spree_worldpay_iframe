486564d3db0606c53a6083258aa02372d0f12089
kaushal <kaushal@supernova.xyz>
2018-01-10 11:43:24 +0530

    Updated state update method to resolve issue on capture

--- app/models/spree/worldpay_notification.rb
+++ app/models/spree/worldpay_notification.rb
@@ -16,35 +16,33 @@ module Spree
       begin
         xml_response = Nokogiri::XML(self.response)
         order_code = xml_response.at_xpath('//orderStatusEvent')['orderCode']
-        order_number = order_code.split('-').first
-        order = Spree::Order.find_by_number order_number
-        if order.present?
+        payment = Spree::Payment.where(response_code: order_code).last
+        if payment.present?
+          order = payment.order
           self.event_type = xml_response.at_xpath('//lastEvent').content
           self.order_id = order.id
           if self.event_type == 'AUTHORISED'
             self.event_type = xml_response.at_xpath('//journal')['journalType']
           end
           self.save
+          self.reload
           if self.event_type == 'AUTHORISED' || self.event_type == 'SENT_FOR_AUTHORISATION'
             payment_method = Spree::PaymentMethod.find_by_type('Spree::Gateway::WorldpayIframe')
             payment_method.create_payment_source(order.id, self.event_type, xml_response)
+            payment.reload
             if self.event_type == 'AUTHORISED' 
-              payment = Spree::Payment.where(response_code: order_code).last
-              payment.capture! if payment.present?
+              payment.capture!
             end
           elsif self.event_type == 'CAPTURED'
-            payment = Spree::Payment.where(response_code: order_code).last
-            if payment.present?
-              if payment.source_id.blank?
-                notify = Spree::WorldpayNotification.where(order_id: order.id, event_type: ['AUTHORISED','SENT_FOR_AUTHORISATION']).last
-                if notify.present?
-                  notify.handle!
-                  payment.reload
-                end
+            if payment.source_id.blank?
+              notify = Spree::WorldpayNotification.where(order_id: order.id, event_type: ['AUTHORISED','SENT_FOR_AUTHORISATION']).last
+              if notify.present?
+                notify.handle!
+                payment.reload
               end
-              payment.capture! if payment.state != 'completed'
-              payment.update(worldpay_capture: true)
             end
+            payment.capture! if payment.state != 'completed'
+            payment.update(worldpay_capture: true)
           end
         end
       rescue Exception => e
