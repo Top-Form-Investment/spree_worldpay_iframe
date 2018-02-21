89b79d7c0ebddfe7380ec01cbc526de95c60c305
kaushal <kaushal@supernova.xyz>
2018-02-21 09:18:09 +0530

    Resolve payment page sequence issue

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,7 +4,7 @@ Spree::Order.class_eval do
 
   # Return available payment methods
   def available_payment_methods
-    @available_payment_methods ||= Spree::PaymentMethod.where(active: true, display_on: ['both', 'front_end']).order(:updated_at)
+    @available_payment_methods ||= Spree::PaymentMethod.where(active: true, display_on: ['both', 'front_end', '']).order(:updated_at)
     country_iso = self.billing_address.country.iso3
     @available_payment_methods.select{|s| s.type != 'Spree::Gateway::WorldpayIframe' || s.eligible?(self.currency, country_iso)}
   end
